5285f988e532959ba348b6e45367bee96f30b318
Irisawa Aoi <aoi.i0924@icloud.com>
2020-05-22 18:16:34 +0900

    2回登録

--- php01/post_confirm.php
+++ php01/post_confirm.php
@@ -1,5 +1,24 @@
 <?php
+function h($value){
+    return htmlspecialchars($value, ENT_QUOTES);
+}
 
+
+
+
+$name = $_POST["name"];
+$mail = $_POST["mail"];
+
+$a = "$name,$mail";
+
+
+
+
+
+//File書き込み
+$file = fopen("data/data.txt","a");	// ファイル読み込み
+fwrite($file, $a."\r\n");
+fclose($file);
 
 
 
@@ -10,8 +29,8 @@
 <title>POST（受信）</title>
 </head>
 <body>
-お名前：
-EMAIL：
+お名前：<?php echo h($name); ?>
+EMAIL：<?php echo h($mail); ?>
 <ul>
 <li><a href="index.php">index.php</a></li>
 </ul>
